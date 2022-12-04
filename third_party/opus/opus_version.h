2415690a97dcf3bdc49f735f9e6dfe169dbe7fc2
depthlove <depthlove@163.com>
2022-12-04 13:57:06 +0800

    Add: opus lib

---
+++ third_party/opus/opus_version.h
@@ -0,0 +1,5 @@
+/* Opus version description
+ * repository address : https://gitlab.xiph.org/xiph/opus/-/tree/v1.3.1
+ * version : 1.3.1
+ * git commit : v1.3.1 e85ed7726db5d677c9c0677298ea0cb9c65bdd23
+ * */
